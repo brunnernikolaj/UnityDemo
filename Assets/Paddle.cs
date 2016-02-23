75db749af581c5de8cdb955efd5aa11cc9c92946
brunnernikolaj <brunnernikolaj@gmail.com>
2016-02-23 08:40:22 +0100

    comment

--- Assets/Paddle.cs
+++ Assets/Paddle.cs
@@ -14,7 +14,7 @@ public class Paddle : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-
+        //Comment
         var newXPos = transform.position.x + (Input.GetAxis("Horizontal") * speed);
 
         var scale = Mathf.Clamp((Mathf.Abs(Input.GetAxis("Horizontal")) - 1),0.5f,1.0f);
